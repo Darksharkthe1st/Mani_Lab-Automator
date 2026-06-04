98a9619529ea94784761a551703802f025af2bf1
Farhan Kittur <kitturfarhan@gmail.com>
2026-06-04 15:06:35 -0700

    Claude's fix

--- Magnet Controller/Magnet Controller/New Magnet Code.cpp
+++ Magnet Controller/Magnet Controller/New Magnet Code.cpp
@@ -16,17 +16,28 @@ void sleepCut(int percent);
 string doubleToStr(int decimalPlaces, double d);
 void terminateIfEsc();
 void magnetTo(string current, string voltage);
+void clickAt(POINT p);
+
 int delay = 0;
 
 /*VARIABLES FOR CHANGING*/
-//Minimum 6 seconds, otherwise the program will break.
-int interval = 10000; //Number of milliseconds (in thousandths of seconds) per interval
-//MULTIPLY THE AMOUNT OF SECONDS BY 1000
-string zeroVoltage = "-0.085"; //Voltage at zero gauss
-string zeroCurrent = "4.2"; //Current at zero gauss
-string defaultCurrent = "4.2"; //Current used by default
+// Minimum 6 seconds, otherwise the program will break.
+int interval = 10000; // milliseconds per interval
+string zeroVoltage = "-0.085"; // Voltage at zero gauss
+string zeroCurrent = "4.2";    // Current at zero gauss
+string defaultCurrent = "4.2"; // Current used by default
 
-//Calculate voltage from Gauss
+// Offsets from SET button to each field (based on lab measurements)
+// SET is at X=732, VSET center is at X=639, ISET center is at X=831
+// So VSET is 93px left of SET, ISET is 99px right of SET
+const int VSET_OFFSET_X = -93;
+const int ISET_OFFSET_X = 99;
+const int FIELD_OFFSET_Y = 0; // All fields are on the same row as SET
+
+// Recorded position of the SET button — filled in when user presses F12
+POINT setButtonPos;
+
+// Calculate voltage from Gauss
 double voltageFunction(int gauss) {
 	return (gauss - 21.1) / 252.34;
 }
@@ -39,6 +50,19 @@ start:
 	double inputGauss;
 	string binaryString;
 	double endingGauss;
+
+	// Ask user to place mouse on SET button and press F12 to record position
+	cout << "Place your mouse cursor directly on the SET button in Kepco, then press F12.\n";
+	while (true) {
+		if (GetAsyncKeyState(VK_F12) & 0x8000) {
+			GetCursorPos(&setButtonPos);
+			cout << "SET button recorded at: " << setButtonPos.x << ", " << setButtonPos.y << "\n";
+			Sleep(300); // debounce
+			break;
+		}
+		Sleep(50);
+	}
+
 	cout << "Give me the Value for Gauss: ";
 	cin >> inputGauss;
 	cout << "Give me your interval in milliseconds (must be greater than 7500): ";
@@ -53,22 +77,13 @@ start:
 
 	string inputCurrent = defaultCurrent;
 	cout << "Voltage: " << inputVoltage;
-	cout << "; Current: " << inputCurrent;
+	cout << "; Current: " << inputCurrent << "\n";
 
-	//Uncomment if current needs to be changed
-	/*string magVal2;
-	cout << "Give me the Value for Current: ";
-	cin >> magVal2;*/
-	string magVal1 = inputVoltage;
-	string magVal2 = inputCurrent;
 	Sleep(1000);
-	//::ShowWindow(::GetConsoleWindow(), SW_HIDE);
-	//std::cout << "Hello World!\n";
-	
+
 	Sleep(50);
-	//delay += countSleep(50);
-	if (GetAsyncKeyState(VK_F12)) {
-		::ShowWindow(::GetConsoleWindow(), SW_HIDE); //Hides the black box
+	if (GetAsyncKeyState(VK_F12) & 0x8000) {
+		::ShowWindow(::GetConsoleWindow(), SW_HIDE);
 
 		for (int i = 0; i < binaryString.length(); i++) {
 			if (binaryString[i] == '0') {
@@ -84,81 +99,83 @@ start:
 
 		magnetTo(inputCurrent, endingVoltage);
 
-		::ShowWindow(::GetConsoleWindow(), SW_SHOW); //Shows the black box
+		::ShowWindow(::GetConsoleWindow(), SW_SHOW);
 		cout << "Give me the Value for Gauss: ";
 		cin >> inputGauss;
 		inputVoltage = doubleToStr(4, voltageFunction(inputGauss));
 		inputCurrent = defaultCurrent;
 	}
-	if (GetAsyncKeyState(VK_ESCAPE)) {
+	if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) {
 		keyTap(VK_TAB);
 		cout << "HI\n";
 	}
-	if (GetAsyncKeyState(VK_NUMPAD0)) {
+	if (GetAsyncKeyState(VK_NUMPAD0) & 0x8000) {
 		terminate();
 	}
-	if (GetAsyncKeyState(VK_NUMPAD2)) {
+	if (GetAsyncKeyState(VK_NUMPAD2) & 0x8000) {
 		goto start;
 	}
-	
 }
 
 void magnetTo(string current, string voltage) {
 	delay = 0;
-	/*Now we turn the magnet off*/
-	{//Deletes the previous text
-		keyDown(VK_CONTROL);
-		clickKey('a');
-		keyUp(VK_CONTROL); //Ctrl + a
-		keyDown(VK_DELETE); //keyUp(VK_DELETE); //Delete
 
-		sleepCut(16);
-		//Types in the voltage
-		sendString(voltage);
+	// Build field positions from the recorded SET button position
+	POINT vsetPos, isetPos, setPos;
+	vsetPos.x = setButtonPos.x + VSET_OFFSET_X;
+	vsetPos.y = setButtonPos.y + FIELD_OFFSET_Y;
+	isetPos.x = setButtonPos.x + ISET_OFFSET_X;
+	isetPos.y = setButtonPos.y + FIELD_OFFSET_Y;
+	setPos = setButtonPos;
 
-		sleepCut(16);
-		//Goes to the current box
-		clickKey('\t');
+	// --- Click into VSET and type voltage ---
+	clickAt(vsetPos);
+	countSleep(50);
+	keyDown(VK_CONTROL);
+	clickKey('a');
+	keyUp(VK_CONTROL);
+	keyDown(VK_DELETE);
+	countSleep(16);
+	sendString(voltage);
+	countSleep(16);
 
-		//Deletes the previous text
-		keyDown(VK_CONTROL);
-		clickKey('a');
-		keyUp(VK_CONTROL); //Ctrl + a
-		keyDown(VK_DELETE); //keyUp(VK_DELETE); //Delete
+	// --- Click into ISET and type current ---
+	clickAt(isetPos);
+	countSleep(50);
+	keyDown(VK_CONTROL);
+	clickKey('a');
+	keyUp(VK_CONTROL);
+	keyDown(VK_DELETE);
+	countSleep(16);
+	sendString(current);
+	countSleep(16);
 
-		sleepCut(16);
-		//Types in the current
-		sendString(current);
+	// --- Click SET button ---
+	clickAt(setPos);
+	countSleep(16);
 
-		//Goes to and hits the magnet switch button
-		clickKey('\t');
-		keyTap(VK_RETURN);
+	// --- Return focus to VSET so next iteration starts consistently ---
+	clickAt(vsetPos);
+	countSleep(16);
 
-		sleepCut(16);
-		/*Clear the error message screen*/
-		clickKey('\t');
-		keyTap(VK_RETURN);
-
-		sleepCut(16);
-		/*Go back to the original location*/
-		for (int tab = 0; tab < 14; tab++) {
-			keyTap(VK_TAB);
-		}
-		sleepCut(16);
-		clickKey('x'); clickKey('\b');
-	}
 	if (delay > interval) {
 		delay = interval;
 	}
 	Sleep(interval - delay);
 }
 
-void clickKey(char c) {
+void clickAt(POINT p) {
+	SetCursorPos(p.x, p.y);
+	mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+	Sleep(30);
+	mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+}
 
+void clickKey(char c) {
 	INPUT Input = { 0 };
 	Input.type = INPUT_KEYBOARD;
 	Input.ki.dwFlags = 0;
-	Input.ki.wVk = VkKeyScanA(c); //Character to output
+	Input.ki.wVk = VkKeyScanA(c);
 	SendInput(1, &Input, sizeof(Input));
 	ZeroMemory(&Input, sizeof(Input));
 
@@ -171,8 +188,8 @@ void clickKey(char c) {
 void keyDown(short s) {
 	INPUT Input = { 0 };
 	Input.type = INPUT_KEYBOARD;
-	Input.ki.wVk = s; //Does the command connected to the short
-	Input.ki.dwFlags = 0; //Key Pressed down
+	Input.ki.wVk = s;
+	Input.ki.dwFlags = 0;
 	SendInput(1, &Input, sizeof(Input));
 	ZeroMemory(&Input, sizeof(Input));
 }
@@ -180,8 +197,8 @@ void keyDown(short s) {
 void keyUp(short s) {
 	INPUT Input = { 0 };
 	Input.type = INPUT_KEYBOARD;
-	Input.ki.wVk = s; //Does the command connected to the short
-	Input.ki.dwFlags = KEYEVENTF_KEYUP; //Key Pressed down
+	Input.ki.wVk = s;
+	Input.ki.dwFlags = KEYEVENTF_KEYUP;
 	SendInput(1, &Input, sizeof(Input));
 	ZeroMemory(&Input, sizeof(Input));
 }
@@ -190,13 +207,11 @@ void keyTap(short s) {
 	keyUp(s); keyDown(s);
 }
 
-//returns total delay caused
 void sendString(string s) {
 	for (int i = 0; i < s.length(); i++) {
 		Sleep(10);
 		clickKey(s[i]);
 	}
-	//return s.length() * 10;
 }
 
 void countSleep(DWORD dwMilliseconds) {
@@ -207,10 +222,9 @@ void countSleep(DWORD dwMilliseconds) {
 
 void sleepCut(int percent) {
 	terminateIfEsc();
-	Sleep(interval * (percent) / 100);
-	delay += interval * (percent) / 100;
+	Sleep(interval * percent / 100);
+	delay += interval * percent / 100;
 }
-
 
 string doubleToStr(int decimalPlaces, double d) {
 	d = round(d * 10000) / 10000;
